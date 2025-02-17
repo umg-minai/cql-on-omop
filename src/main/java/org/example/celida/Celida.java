2abbb1c05fe5e16474362251b81133341ef1178d
Jan Moringen <jan.moringen@med.uni-greifswald.de>
2025-02-17 17:54:56 +0100

    Improvements in Celida driver

--- src/main/java/org/example/celida/Celida.java
+++ src/main/java/org/example/celida/Celida.java
@@ -7,6 +7,8 @@ import org.example.engine.ConnectionFactory;
 import org.example.engine.InMemoryLibrarySourceProvider;
 import org.hl7.elm.r1.VersionedIdentifier;
 
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Celida {
@@ -24,15 +26,15 @@ public class Celida {
     }
 
     private void run() {
-        getPersonStream().forEach(this::evaluatePerson);
+        getPersons().forEach(this::evaluatePerson);
     }
 
-    private Stream<Person> getPersonStream() {
+    private Set<Person> getPersons() {
         final var clazz = OMOP.Person.class;
         final var criteria = entityManager.getCriteriaBuilder()
                 .createQuery(clazz);
         final var query = criteria.select(criteria.from(clazz));
-        return entityManager.createQuery(query).getResultStream();
+        return entityManager.createQuery(query).getResultStream().collect(Collectors.toSet());
     }
 
     private void evaluatePerson(final OMOP.Person person) {
@@ -42,9 +44,40 @@ public class Celida {
         librarySourceProvider.registerLibrary(identifier, String.format("""
                         library %s version '0.1.0'
                         using OMOP version 'v5.4'
+                        include "OMOPHelpers"
+
+                        codesystem OMOPCode: 'https://fhir-terminology.ohdsi.org'
+                        code "Condition COVID19 via OMOP": '37311061' from OMOPCode display 'Disease caused by Severe acute respiratory syndrome coronavirus 2 (disorder)'
+
+                        codesystem SNOMED: 'http://snomed.info/sct'
+                        code "Condition COVID19 via SNOMED": '840539006' from SNOMED display 'Disease caused by Severe acute respiratory syndrome coronavirus 2 (disorder)'
+
+                        concept "Condition COVID19": {
+                           "Condition COVID19 via OMOP",
+                           "Condition COVID19 via SNOMED"
+                        }
+
                         context Patient
-                        define E: [ConditionOccurrence]
-                        define F: [Observation] o return o.observationDateTime""", libraryName)
+                        //define Covid: First([Concept: Code '111293003' from SNOMED])
+                        //define Birth: Patient.birthDatetime
+                        //define AgeInYearsToday: AgeInYearsAt(@2025-02-13)
+                        //define Conditions: [ConditionOccurrence] c return {name: c.conditionConcept.conceptName, code: c.conditionConcept.conceptId}
+                        //define F: [Observation] o return o.observationDatetime
+                        //define G: [Observation] o return o.observationDate
+                        define function PatientAndCondition(condition OMOP.ConditionOccurrence):
+                          condition c
+                          let con: c.conditionConcept
+                          return {
+                            person: c.person,
+                            age: AgeInYearsAt(@2025-02-13),
+                            conceptName: con.conceptName,
+                            conceptId: con.conceptId,
+                            conceptCode: con.conceptCode + ' (from ' + con.vocabularyId + ')'
+                          }
+                        define ViaSNOMED: [ConditionOccurrence: "Condition COVID19 via SNOMED"] c return all PatientAndCondition(c)
+                        define ViaOMOP: [ConditionOccurrence: conditionConcept ~ "Condition COVID19 via OMOP"] c return all PatientAndCondition(c)
+                        //define COVID: [ConditionOccurrence] c where c.conditionConcept subsumes ToConcept("Condition COVID19")
+                        """, libraryName)
                 );
         final var result = this.engine.evaluateLibrary(libraryName, person);
         System.out.printf("%s%n", person);
@@ -56,9 +89,11 @@ public class Celida {
 
     private void presentResultValue(final Object value) {
         if (value instanceof Iterable<?> iterable) {
+            System.out.print("{");
             for (var element : iterable) {
                 System.out.printf("%s, ", element);
             }
+            System.out.print("}");
         } else {
             System.out.print(value);
         }
