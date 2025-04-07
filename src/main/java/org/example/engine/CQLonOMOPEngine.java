e6f08c9285c83f7c77f1b9f4c6278e5c86a38921
Jan Moringen <jan.moringen@med.uni-greifswald.de>
2025-04-07 15:59:49 +0200

    Experimental code for initializing Hibernate proxies
    Should not be when operating in some batch mode. On the other hand,
    for interactive use, the problem is that printing evaluation results
    accesses fields of Hibernate proxies which leads to attempted database
    queries after the Hibernate session is no longer active.

--- src/main/java/org/example/engine/CQLonOMOPEngine.java
+++ src/main/java/org/example/engine/CQLonOMOPEngine.java
@@ -5,14 +5,17 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.cqframework.cql.cql2elm.*;
 import org.hibernate.Hibernate;
 import org.hibernate.SessionFactory;
+import org.hibernate.proxy.HibernateProxy;
 import org.opencds.cqf.cql.engine.data.DataProvider;
 import org.opencds.cqf.cql.engine.execution.CqlEngine;
 import org.opencds.cqf.cql.engine.execution.Environment;
 import org.opencds.cqf.cql.engine.execution.EvaluationResult;
 import org.opencds.cqf.cql.engine.terminology.TerminologyProvider;
 
+import java.beans.BeanDescriptor;
+import java.beans.PropertyDescriptor;
 import java.nio.file.Path;
-import java.util.Map;
+import java.util.*;
 
 public class CQLonOMOPEngine {
 
@@ -83,15 +86,64 @@ public class CQLonOMOPEngine {
             final var environment = new Environment(this.libraryManager, dataProviders, this.terminologyProvider);
             //engine.getState().getDebugResult().getMessages().forEach(System.err::println);
             final var engine = new CqlEngine(environment);
-            if (contextObject == null) {
-                return engine.evaluate(library, parameterBindings);
-            } else {
-                // TODO(jmoringe): can we know the name of the context?
-                return engine.evaluate(library,
-                        Pair.of("Patient", contextObject),
+            final var result = contextObject == null
+                ? engine.evaluate(library, parameterBindings)
+                : engine.evaluate(library,
+                        Pair.of("Patient", contextObject), // TODO(jmoringe): can we know the name of the context?
                         parameterBindings);
-            }
+            result.expressionResults.forEach((name, expressionResult) -> {
+                initializeProxies(expressionResult.value(), new HashSet<>());
+            });
+            return result;
         }
+    }
+
+    private void initializeProxies(final Object object, final HashSet<Object> seen) {
+        object.toString(); // HACK(jmoringe): force some initialization
+
+        /*if (!seen.contains(object)) {
+            System.out.printf("Checking %s\n", object);
+            seen.add(object);
+            if (object instanceof HibernateProxy proxy) {
+                System.out.printf("Is a proxy %s\n", object);
+                if (!Hibernate.isInitialized(proxy)) {
+                    System.out.printf("Is not initialized %s\n", object);
+                    Hibernate.initialize(proxy);
+                } else {
+                    System.out.printf("Is initialized %s\n", object);
+                }
+                /final var d = new BeanDescriptor(proxy.getClass());
+                if (proxy instanceof Collection<?> collection) {
+                    collection.forEach(element -> initializeProxies(element, seen));
+                }/
+            } else if (object instanceof Iterable<?> iterable) {
+                for (var element : iterable) {
+                    initializeProxies(element, seen);
+                }
+            } else if (object instanceof Optional<?> optional) {
+                optional.ifPresent(value -> initializeProxies(value, seen));
+            } else {
+                System.out.printf("Not a proxy %s\n", object);
+            }
+            if (object.getClass().getPackageName().contains("OMOP")) {
+                // TODO(jmoringe): copied from ResultPresenter
+                final var clazz = object.getClass();
+                Arrays.stream(clazz.getMethods())
+                        .filter(method -> method.getName().startsWith("get")
+                                && !method.getName().equals("getClass"))
+                        .forEach(method -> {
+                            final var fieldName = method.getName().substring(3, 4).toLowerCase(Locale.ROOT)
+                                    + method.getName().substring(4);
+                            Object fieldValue;
+                            try {
+                                fieldValue = method.invoke(object);
+                            } catch (Exception e) {
+                                fieldValue = String.format("error accessing field: %s", e);
+                            }
+                            initializeProxies(fieldValue, seen);
+                        });
+            }
+        }*/
     }
 
     public EvaluationResult evaluateLibrary(final String library, final Object contextObject) {
