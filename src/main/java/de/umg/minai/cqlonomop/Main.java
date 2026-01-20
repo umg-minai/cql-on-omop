bd86c6a8a1e38f7ad89a1d8bc4a204160580ffed
Jan Moringen <jan.moringen@med.uni-greifswald.de>
2026-01-20 16:33:30 +0100

    Simplify sub-command processing in Main

--- src/main/java/de/umg/minai/cqlonomop/Main.java
+++ src/main/java/de/umg/minai/cqlonomop/Main.java
@@ -6,20 +6,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
 
-import java.util.List;
-import java.util.concurrent.Callable;
 import java.util.logging.Level;
 
 @CommandLine.Command(
         name = "cql-on-omop",
         version = "cql-on-omop 0.1",
-        subcommands = {
-                Batch.class,
-                Repl.class
-        },
+        subcommands = { Batch.class, Repl.class },
         mixinStandardHelpOptions = true
 )
-public class Main implements Callable<Integer> {
+public class Main {
 
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
@@ -28,12 +23,6 @@ public class Main implements Callable<Integer> {
         logManager.getLogger("").setLevel(Level.WARNING);
         final var exitCode = new CommandLine(new Main()).execute(args);
         System.exit(exitCode);
-    }
-
-    @Override
-    public Integer call() {
-        System.out.printf("Available sub-commands: %s%n", List.of("repl, main")); // TODO(jmoringe): obtain this programatically
-        return CommandLine.ExitCode.USAGE;
     }
 
 }
